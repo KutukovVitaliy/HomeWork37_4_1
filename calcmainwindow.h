ee90a80ad894f9f83ee01f4b2af453c0503097f6
kvit <kvit.vn@gmail.com>
2022-04-08 15:21:25 +0300

    next commit

--- calcmainwindow.h
+++ calcmainwindow.h
@@ -6,6 +6,14 @@
 #define HOMEWORK37_4_1_CALCMAINWINDOW_H
 #include <QMainWindow>
 #include <QtWidgets/QLineEdit>
+#include <exception>
+
+class DivByZeroException : public std::exception{
+public:
+    const char* what() const noexcept override{
+        return "Division by zero! Not allowed!";
+    }
+};
 
 class CalcMainWindow : public QMainWindow{
     Q_OBJECT
@@ -15,18 +23,35 @@ public:
 
 public slots:
     void plus(){
-        resultLineEdit->setText(QString::number(firstLineEdit->text().toFloat() + secondLineEdit->text().toFloat()));
+        float result = firstLineEdit->text().replace(",", ".").toFloat() + secondLineEdit->text().replace(",", ".").toFloat();
+        resultLineEdit->setText(QString::number(result).replace(".", ","));
 
     };
     void minus(){
-
+        float result = firstLineEdit->text().replace(",", ".").toFloat() - secondLineEdit->text().replace(",", ".").toFloat();
+        resultLineEdit->setText(QString::number(result).replace(".", ","));
     }
     void mul(){
-
+        float result = firstLineEdit->text().replace(",", ".").toFloat() * secondLineEdit->text().replace(",", ".").toFloat();
+        resultLineEdit->setText(QString::number(result).replace(".", ","));
     }
     void div(){
 
+            float first = firstLineEdit->text().replace(",", ".").toFloat();
+            float second = secondLineEdit->text().replace(",", ".").toFloat();
+        try {
+            float result = Division(first, second);
+            resultLineEdit->setText(QString::number(result).replace(".", ","));
+        }
+        catch (DivByZeroException ex) {
+            resultLineEdit->setText(ex.what());
+        }
     }
 
+private:
+    float Division(float first, float second){
+        if(second == 0) throw DivByZeroException();
+        return first / second;
+    }
 };
 #endif //HOMEWORK37_4_1_CALCMAINWINDOW_H
